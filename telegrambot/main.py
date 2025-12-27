d4ff5937897c66b69fe1ddbd0af62aa1c8a42016
Alireza-Kianfar <akgalireza@gmail.com>
2025-12-27 18:37:46 +0330

    welcome message on /start

---
+++ telegrambot/main.py
@@ -0,0 +1,12 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+BOT_TOKEN = "Token"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Salam, in task man hast baraye Python Developer, Khosh Amadid!")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
