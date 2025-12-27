dec377a02567d3b5c0e8d0b2d6b20fb9964444c0
Alireza-Kianfar <akgalireza@gmail.com>
2025-12-27 18:46:19 +0330

    add send user message

--- telegrambot/main.py
+++ telegrambot/main.py
@@ -1,12 +1,20 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler,MessageHandler , ContextTypes, filters
 
-BOT_TOKEN = "Token"
+BOT_TOKEN = "TOKEN"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salam, in task man hast baraye Python Developer, Khosh Amadid!")
 
+
+async def messagehandler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+    await update.message.reply_text(user_message)
+
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+
+filter_for_text = filters.TEXT & (~filters.COMMAND)
+app.add_handler(MessageHandler(filter_for_text, messagehandler))
 
 app.run_polling()
